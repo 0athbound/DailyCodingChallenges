ff61d566e6cc2806d35808d4300d3f69d0d6acb3
Silas Yost <silas.yost@gmail.com>
2022-03-12 13:51:08 -0500

    Add coding challenge and Solution from 3-12-2022

---
+++ CC 2022-03-12/app.js
@@ -0,0 +1,20 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//Parameters: Assuming only a Boolean value will be input
+
+//Results: Returns "true" or "false" depending on boolean
+
+//Examples:
+
+//Pseudocode: create a function to take in a boolean and return a string
+//            try using .toString() method
+
+function booleanToString(b){
+    if(b) {
+        return 'true';
+    }else {
+        return 'false';
+    }
+  }
